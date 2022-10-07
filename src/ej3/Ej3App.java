c887168427019d6a0ced460a86ffa96e9e12eb15
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-10-07 16:48:18 +0200

    Packages

---
+++ src/ej3/Ej3App.java
@@ -0,0 +1,10 @@
+package ej3;
+
+public class Ej3App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ej6/Ej6App.java
@@ -0,0 +1,10 @@
+package ej6;
+
+public class Ej6App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
