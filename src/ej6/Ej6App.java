02457c55d33a29efdab74717d460d2ee891e3283
ivanhrt <ivanherreror@gmail.com>
2022-10-12 21:02:50 +0200

    Update Ej6App.java

--- src/ej6/Ej6App.java
+++ src/ej6/Ej6App.java
@@ -4,7 +4,7 @@ public class Ej6App {
 
 	public static void main(String[] args) {
 		Pelicula pelicula = new Pelicula("Indiana Jones", 7.56,153,7,"Spielberg");
-		Sala sala = new Sala(3,2);
+		Sala sala = new Sala(2,2);
 		Espectador espectador_1 = new Espectador("Juan",6,50.5);
 		Espectador espectador_2 = new Espectador("Pepe",23,6);
 		Espectador espectador_3 = new Espectador("Eustaquio",18,50.5);
