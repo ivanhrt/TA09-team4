5de3374242a3f96c42fe185e71aa5b721bb70794
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-10-12 18:22:43 +0200

    Update ej6
    Se han introducido los filtros dentro del metodo ocuparAsiento de la clase Sala, tal y como indico Jose

--- src/ej6/Ej6App.java
+++ src/ej6/Ej6App.java
@@ -4,13 +4,7 @@ public class Ej6App {
 
 	public static void main(String[] args) {
 		Pelicula pelicula = new Pelicula("Indiana Jones", 7.56,153,7,"Spielberg");
-<<<<<<< Updated upstream
-		Sala sala = new Sala(2,2);
-=======
 		Sala sala = new Sala(3,2);
-		
-		//Pruebas para llenar la sala de espectadores
->>>>>>> Stashed changes
 		Espectador espectador_1 = new Espectador("Juan",6,50.5);
 		Espectador espectador_2 = new Espectador("Pepe",23,6);
 		Espectador espectador_3 = new Espectador("Eustaquio",18,50.5);
@@ -18,38 +12,13 @@ public class Ej6App {
 		Espectador espectador_5 = new Espectador("Stefania",25,50.5);
 		Espectador espectador_6 = new Espectador("Jennifer",30,50.5);
 		Espectador espectador_7 = new Espectador("Sonia",45,50.5);
-		
-		llenarSala(pelicula, espectador_1, sala);
-		llenarSala(pelicula, espectador_2, sala);
-		llenarSala(pelicula, espectador_3, sala);
-		llenarSala(pelicula, espectador_4, sala);
-		llenarSala(pelicula, espectador_5, sala);
-		llenarSala(pelicula, espectador_6, sala);
-		llenarSala(pelicula, espectador_7, sala);
-		
-	}
-
-	public static void llenarSala(Pelicula pelicula, Espectador espectador, Sala sala) {
-		
-		//Impido acceder a un menor de edad saliendo del metodo si no pasa el if
-		if (espectador.getEdad()<pelicula.getEdad_min()) {
-			System.out.println(espectador.getNombre()+" No tiene la edad mínima");
-			return;
-		}
-		
-		//Impido acceder si no tiene suficiente dinero saliendo del metodo si no pasa el if
-		if (espectador.getDinero()<pelicula.getPrecio_entrada()) {
-			System.out.println(espectador.getNombre()+" No tiene suficiente dinero");
-			return;
-		}
-		
-		if(sala.contarLLeno()) {
-			System.out.println(espectador.getNombre()+" La sala ya esta llena");
-			return;
-		}
-		
-		System.out.println(espectador.getNombre()+" Tu asiento para la pelicula "+pelicula.getPelicula()+" es:");
-		sala.ocuparAsiento();
+		sala.ocuparAsiento(espectador_1, pelicula);
+		sala.ocuparAsiento(espectador_2, pelicula);
+		sala.ocuparAsiento(espectador_3, pelicula);
+		sala.ocuparAsiento(espectador_4, pelicula);
+		sala.ocuparAsiento(espectador_5, pelicula);
+		sala.ocuparAsiento(espectador_6, pelicula);
+		sala.ocuparAsiento(espectador_7, pelicula);
 	}
 	
 }
--- src/ej6/Sala.java
+++ src/ej6/Sala.java
@@ -3,20 +3,22 @@ package ej6;
 import java.util.Random;
 
 public class Sala {
-	
+
 	private int filas;
 	private int columnas;
 	private int[][] sala;
-	
+
 	public Sala() {
-		this.filas=0;
-		this.columnas=0;
+		this.filas = 0;
+		this.columnas = 0;
+		this.sala = new int[this.filas][this.columnas];
 	}
-	
+
 	public Sala(int filas, int columnas) {
 		this.filas = filas;
 		this.columnas = columnas;
-		//Genero una matriz de numero de filas i numero de columnas que representan los asientos del cine
+		// Genero una matriz de numero de filas i numero de columnas que representan los
+		// asientos del cine
 		this.sala = new int[this.filas][this.columnas];
 	}
 
@@ -35,46 +37,72 @@ public class Sala {
 	public void setColumnas(int columnas) {
 		this.columnas = columnas;
 	}
-	
+
 	public int[][] getSala() {
 		return sala;
 	}
-	
-	
-	
-	//Metodo que cuenta cuantos huecos tienen el String Ocupado, si todos los huecos tienen este String, entonces el cine esta lleno
+
+	// contar el numero de huecos que hay en la matriz
+	public double contarAsientos() {
+		double n_asientos = this.columnas * this.filas;
+		return n_asientos;
+	}
+
+	// Metodo que cuenta cuantos huecos tienen el String Ocupado, si todos los
+	// huecos tienen este String, entonces el cine esta lleno
 	public boolean contarLLeno() {
-		//constante donde se le suma uno por cada espacio que tenga el string Ocupado 
-		int contador=0;
-		for (int i=0;i<this.filas;i++) {
-			for (int j=0; j<this.columnas;j++) {
-				if (this.sala[i][j]==1) {
+		// constante donde se le suma uno por cada espacio que tenga el string Ocupado
+		int contador = 0;
+		for (int i = 0; i < this.filas; i++) {
+			for (int j = 0; j < this.columnas; j++) {
+				if (this.sala[i][j] == 1) {
 					contador++;
 				}
 			}
 		}
-		if (contador==(this.filas*this.columnas)) {
+		if (contador == (this.filas * this.columnas)) {
 			return true;
 		}
 		return false;
 	}
-	
-	//Ocupar un asiento
-	public void ocuparAsiento() {
+
+	// Ocupar un asiento
+	public void ocuparAsiento(Espectador espectador, Pelicula pelicula) {
+		
+		// Impido acceder a un menor de edad saliendo del metodo si no pasa el if
+		if (espectador.getEdad() < pelicula.getEdad_min()) {
+			System.out.println("No tiene la edad mínima");
+			return;
+		}
+
+		// Impido acceder si no tiene suficiente dinero saliendo del metodo si no pasa
+		// el if
+		if (espectador.getDinero() < pelicula.getPrecio_entrada()) {
+			System.out.println("No tiene suficiente dinero");
+			return;
+		}
+
+		if (contarLLeno()) {
+			System.out.println("La sala ya esta llena");
+			return;
+		}
+
+		System.out.println(espectador.getNombre()+" tu asiento para la pelicula " + pelicula.getPelicula() + " es:");
 		Random rad = new Random();
 		Random rad_1 = new Random();
-		int n_filas=0;
+		int n_filas = 0;
 		int n_columna = 0;
 		do {
-			//cojo numero random para la fila y para la columna y miro si tiene el string Ocupado, en caso positivo entonces busca otro lugar
+			// cojo numero random para la fila y para la columna y miro si tiene el string
+			// Ocupado, en caso positivo entonces busca otro lugar
 			n_filas = rad.nextInt(this.filas);
 			n_columna = rad_1.nextInt(this.columnas);
-		}while(this.sala[n_filas][n_columna]==1);
-		//el lugar assignado pasa a guardar el asiento Ocupado
-		this.sala[n_filas][n_columna]=1;
-		int numero_columna=n_columna+'A';
-		char letra_columna = (char)numero_columna;
-		System.out.println(""+(this.filas-n_filas)+letra_columna);
-		
+		} while (this.sala[n_filas][n_columna] == 1);
+		int numero_columna = n_columna + 'A';
+		char letra_columna = (char) numero_columna;
+		// el lugar assignado pasa a guardar el String Ocupado
+		this.sala[n_filas][n_columna] = 1;
+		System.out.println("" + (this.filas - n_filas) + letra_columna);
+
 	}
 }
